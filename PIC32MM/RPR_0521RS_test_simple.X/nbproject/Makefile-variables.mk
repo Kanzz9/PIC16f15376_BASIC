a398a405093f7dded05c4324c6a74765662c11c3
aa <cutoanpo1999@gmail.com>
2021-10-07 20:39:30 +0700

    RPR_1

---
+++ PIC32MM/RPR_0521RS_test_simple.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=RPR_0521RS_test_simple.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/RPR_0521RS_test_simple.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=rpr0521rstestsimple.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/rpr0521rstestsimple.x.tar
