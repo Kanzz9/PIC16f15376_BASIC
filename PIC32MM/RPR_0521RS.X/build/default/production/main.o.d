211e05a23b599f0e9b78d54a112591dbe1c923fd
aa <cutoanpo1999@gmail.com>
2021-10-07 22:16:39 +0700

    Update I2C_Using_Simple

--- PIC32MM/RPR_0521RS.X/build/default/production/main.o.d
+++ PIC32MM/RPR_0521RS.X/build/default/production/main.o.d
@@ -2,8 +2,13 @@ build/default/production/main.o: main.c mcc_generated_files/mcc.h \
  mcc_generated_files/system.h mcc_generated_files/clock.h \
  mcc_generated_files/pin_manager.h mcc_generated_files/delay.h \
  mcc_generated_files/interrupt_manager.h mcc_generated_files/exceptions.h \
- mcc_generated_files/uart2.h mcc_generated_files/i2c1.h \
- mcc_generated_files/watchdog.h ../lib/I2C.h ../lib/RPR0521RS.h
+ mcc_generated_files/uart2.h ../lib/RPR0521RS.h ../lib/i2c_using_simple.h \
+ ../lib/../../PIC32MM/RPR_0521RS.X/mcc_generated_files/mcc.h \
+ mcc_generated_files/drivers/../drivers/i2c_master.h \
+ mcc_generated_files/drivers/../drivers/i2c_types.h \
+ mcc_generated_files/drivers/i2c_simple_master.h \
+ mcc_generated_files/drivers/../drivers/i2c_types.h \
+ mcc_generated_files/i2c1_driver.h
 
 mcc_generated_files/mcc.h:
 
@@ -21,10 +26,18 @@ mcc_generated_files/exceptions.h:
 
 mcc_generated_files/uart2.h:
 
-mcc_generated_files/i2c1.h:
-
-mcc_generated_files/watchdog.h:
-
-../lib/I2C.h:
-
 ../lib/RPR0521RS.h:
+
+../lib/i2c_using_simple.h:
+
+../lib/../../PIC32MM/RPR_0521RS.X/mcc_generated_files/mcc.h:
+
+mcc_generated_files/drivers/../drivers/i2c_master.h:
+
+mcc_generated_files/drivers/../drivers/i2c_types.h:
+
+mcc_generated_files/drivers/i2c_simple_master.h:
+
+mcc_generated_files/drivers/../drivers/i2c_types.h:
+
+mcc_generated_files/i2c1_driver.h:
---
+++ PIC32MM/lib/i2c_using_simple.h
@@ -0,0 +1,13 @@
+/*
+ * author: Hoang Khanh Toan
+ * 
+ */
+#include "../../PIC32MM/RPR_0521RS.X/mcc_generated_files/mcc.h"
+#include "mcc.h"
+#ifndef i2c_using_simple_H
+#define i2c_using_simple_H
+
+
+
+
+#endif
