1a4d800741437c011d640f8a194832564c350735
Dang Hoang <hoangvandang12@gmail.com>
2021-10-12 22:36:31 +0700

    mplab ngu

--- PIC32MM/flash.X/nbproject/Makefile-impl.mk
+++ PIC32MM/flash.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Flash_sst_32M.X
+PROJECTNAME=flash.X
 
 # Active Configuration
 DEFAULTCONF=default
--- PIC32MM/flash.X/nbproject/Package-default.bash
+++ PIC32MM/flash.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Flash_sst_32M.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Flash_sst_32M.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=flashsst32m.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/flash.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=flash.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=flash.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/flashsst32m.x/bin
+makeDirectory ${TMPDIR}/flash.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/flashsst32m.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/flash.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/flashsst32m.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/flash.x.tar *
 checkReturnCode
 
 # Cleanup
